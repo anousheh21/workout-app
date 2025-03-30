cc7de71cfd71fe1b999d45352810ca44071b0634
anousheh21 <anousheh1@gmail.com>
2025-03-30 17:35:42 +0100

    Created ExerciseLogSingle component basics

---
+++ app/src/main/java/com/example/workoutapp/ui/components/ExerciseLogSingle.kt
@@ -0,0 +1,9 @@
+package com.example.workoutapp.ui.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ExerciseLogSingle(workout: String) {
+    Text(text = workout)
+}
--- app/src/main/java/com/example/workoutapp/ui/screens/WorkoutDetailScreen.kt
+++ app/src/main/java/com/example/workoutapp/ui/screens/WorkoutDetailScreen.kt
@@ -1,9 +1,22 @@
 package com.example.workoutapp.ui.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.workoutapp.data.Exercise
+import com.example.workoutapp.ui.components.ExerciseLogSingle
 
 @Composable
 fun WorkoutDetailScreen(workout: String) {
-    Text(text = workout)
+    Column {
+        ExerciseLogSingle(workout)
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WorkoutDetailScreenPreview() {
+    WorkoutDetailScreen(workout = "Push Workout")
 }
