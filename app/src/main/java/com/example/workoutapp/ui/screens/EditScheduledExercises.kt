c14daed98b74c905328e5fea1c4d9b59e50887b0
anousheh21 <anousheh1@gmail.com>
2025-04-01 16:19:02 +0100

    Add Exercises

--- app/src/main/java/com/example/workoutapp/ui/screens/EditScheduledExercises.kt
+++ app/src/main/java/com/example/workoutapp/ui/screens/EditScheduledExercises.kt
@@ -5,17 +5,18 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import com.example.workoutapp.ui.components.editschedule.AddNewWorkoutScheduleEdit
 import com.example.workoutapp.ui.components.editschedule.singleWorkout.SingleExerciseScheduleEdit
 
 @Composable
 fun EditScheduledExercises(workoutPlanId: Int) {
-    val scrollState = rememberScrollState() 
+    val scrollState = rememberScrollState()
 
-    Column(
-        modifier = Modifier
-            .verticalScroll(scrollState)
-    ) {
 
-    }
 }
--- app/src/main/java/com/example/workoutapp/ui/screens/PBs.kt
+++ app/src/main/java/com/example/workoutapp/ui/screens/PBs.kt
@@ -1,9 +1,38 @@
 package com.example.workoutapp.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import com.example.workoutapp.ui.components.editschedule.AddNewWorkoutScheduleEdit
+import com.example.workoutapp.ui.components.editschedule.singleWorkout.SingleExerciseScheduleEdit
 
 @Composable
 fun PBs() {
-    Text("PBs")
+    val scrollState = rememberScrollState()
+
+    var nextExerciseId by remember { mutableStateOf(1) }
+    val exerciseIds = remember { mutableStateListOf<Int>() }
+
+    Column(
+        modifier = Modifier
+            .verticalScroll(scrollState)
+    ) {
+
+        exerciseIds.forEach { id ->
+            SingleExerciseScheduleEdit()
+        }
+
+        AddNewWorkoutScheduleEdit(nextExerciseId) { id ->
+            exerciseIds.add(id)
+            nextExerciseId++
+        }
+    }
 }
