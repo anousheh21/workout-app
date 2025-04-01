09e6eac4e6e9c2ed1e29e6e1646258f66b30047f
anousheh21 <anousheh1@gmail.com>
2025-04-01 16:10:48 +0100

    SCroll

--- app/src/main/java/com/example/workoutapp/ui/screens/EditScheduledExercises.kt
+++ app/src/main/java/com/example/workoutapp/ui/screens/EditScheduledExercises.kt
@@ -1,9 +1,21 @@
 package com.example.workoutapp.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.workoutapp.ui.components.editschedule.singleWorkout.SingleExerciseScheduleEdit
 
 @Composable
 fun EditScheduledExercises(workoutPlanId: Int) {
-    Text(text = "$workoutPlanId")
+    val scrollState = rememberScrollState() 
+
+    Column(
+        modifier = Modifier
+            .verticalScroll(scrollState)
+    ) {
+
+    }
 }
