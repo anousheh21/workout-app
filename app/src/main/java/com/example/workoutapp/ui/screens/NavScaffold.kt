13020119eb377e93947ea6029197ac68893f038e
anousheh21 <anousheh1@gmail.com>
2025-04-03 14:13:34 +0100

    Navigate to new workout screen

--- app/src/main/java/com/example/workoutapp/ui/screens/NavScaffold.kt
+++ app/src/main/java/com/example/workoutapp/ui/screens/NavScaffold.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -64,7 +65,12 @@ fun NavScaffold(
                 // ---- TOP BAR for Workouts ----
                 AppScreen.Workouts.route -> {
                     TopAppBar(
-                        title = { Text("Workouts") }
+                        title = { Text("Workouts") },
+                        actions = {
+                            TextButton(onClick = { navController.navigate(AppScreen.NewWorkout.route) }) {
+                                Text("New Workout")
+                            }
+                        }
                     )
                 }
                 // ---- TOP BAR for PBs ----
