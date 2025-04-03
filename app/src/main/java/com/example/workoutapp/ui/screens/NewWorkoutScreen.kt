bd187ff716b0d64c2e8af366ff99418ee2fbf1db
anousheh21 <anousheh1@gmail.com>
2025-04-03 14:23:10 +0100

    Listed workouts to select from

--- app/src/main/java/com/example/workoutapp/ui/screens/NewWorkoutScreen.kt
+++ app/src/main/java/com/example/workoutapp/ui/screens/NewWorkoutScreen.kt
@@ -1,9 +1,46 @@
 package com.example.workoutapp.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.workoutapp.data.ScheduledWorkoutExercise
+import com.example.workoutapp.data.ScheduledWorkoutWithExercises
+import com.example.workoutapp.ui.WorkoutViewModel
+import com.example.workoutapp.ui.theme.SeparatorGrey
 
 @Composable
 fun NewWorkoutScreen() {
-    Text("New Workout Screen")
+    val scrollState = rememberScrollState()
+    val vm: WorkoutViewModel = viewModel()
+    val context = LocalContext.current
+
+    LaunchedEffect(Unit) {
+        vm.loadScheduledWorkoutsWithExercises(context)
+    }
+
+    val schedWorkouts = vm.scheduledWorkoutsWithExercises
+
+    Column(
+        modifier = Modifier
+            .verticalScroll(scrollState)
+    ) {
+        schedWorkouts.forEach { item ->
+            SelectWorkoutRow(schedWorkout = item)
+
+            Divider(color = SeparatorGrey, thickness = 1.dp)
+        }
+    }
+}
+
+@Composable
+fun SelectWorkoutRow(schedWorkout: ScheduledWorkoutWithExercises) {
+    Text(text = schedWorkout.workoutName)
 }
