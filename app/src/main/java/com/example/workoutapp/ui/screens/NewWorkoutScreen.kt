9e0a472f3a1048c3f3aaa5d800001eb637b31922
anousheh21 <anousheh1@gmail.com>
2025-04-05 15:56:52 +0100

    More styling

--- app/src/main/java/com/example/workoutapp/ui/screens/NewWorkoutScreen.kt
+++ app/src/main/java/com/example/workoutapp/ui/screens/NewWorkoutScreen.kt
@@ -1,9 +1,14 @@
 package com.example.workoutapp.ui.screens
 
 import androidx.compose.foundation.gestures.snapping.SnapPosition
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,10 +20,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -95,10 +102,24 @@ fun SelectWorkoutRow(
         schedWorkout: ScheduledWorkoutWithExercises,
         startWorkout: () -> Unit,
     ) {
-    Row {
-        Text(text = schedWorkout.workoutName)
+    Spacer(modifier = Modifier.height(11.dp))
+    Row(
+       modifier = Modifier
+           .fillMaxWidth()
+           .padding(horizontal = 32.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = schedWorkout.workoutName,
+            style = TextStyle(
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Medium
+            )
+        )
         StartWorkoutButton(startWorkout)
     }
+    Spacer(modifier = Modifier.height(11.dp))
 }
 
 @Composable
@@ -109,7 +130,7 @@ fun StartWorkoutButton(
         onClick = { startWorkout() },
         shape = RoundedCornerShape(10.dp),
         colors = ButtonDefaults.buttonColors(containerColor = PrimaryColor),
-        contentPadding = PaddingValues(start = 16.dp, end = 16.dp, top = 8.dp, bottom = 8.dp),
+        contentPadding = PaddingValues(start = 16.dp, end = 16.dp, top = 5.dp, bottom = 5.dp),
         modifier = Modifier
             .width(65.dp)
     ) {
