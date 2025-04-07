9b65eec93d305483f4b3375dacbf11f4d36e7922
anousheh21 <anousheh1@gmail.com>
2025-04-07 09:26:51 +0100

    Content provider onCreate function

--- app/src/main/java/com/example/workoutapp/provider/WorkoutContentProvider.kt
+++ app/src/main/java/com/example/workoutapp/provider/WorkoutContentProvider.kt
@@ -4,10 +4,18 @@ import android.content.ContentProvider
 import android.content.ContentValues
 import android.database.Cursor
 import android.net.Uri
+import androidx.room.Database
+import com.example.workoutapp.data.DatabaseProvider
+import com.example.workoutapp.data.WorkoutDao
 
 class WorkoutContentProvider: ContentProvider() {
+    private lateinit var workoutDao: WorkoutDao
+
     override fun onCreate(): Boolean {
-        TODO("Not yet implemented")
+        // Get an instance of the workoutDAO, to use the Room database
+        workoutDao = DatabaseProvider.getDatabase(context!!).workoutDao()
+        // If provider was loaded successfully, return true
+        return true
     }
 
     override fun query(
