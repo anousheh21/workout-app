4a90a42a18f2a288b27c83832b3a345b47d5574f
anousheh21 <anousheh1@gmail.com>
2025-04-16 15:52:27 +0100

    Button -> TextButton

--- app/src/main/java/com/example/workoutapp/ui/components/editschedule/AddNewWorkoutScheduleEdit.kt
+++ app/src/main/java/com/example/workoutapp/ui/components/editschedule/AddNewWorkoutScheduleEdit.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
@@ -19,20 +20,20 @@ fun AddNewWorkoutScheduleEdit(
     id: Int,
     onAddNewRow: (Int) -> Unit
 ) {
-    Button(
+    TextButton(
         onClick = { onAddNewRow(id) },
-        shape = RoundedCornerShape(10.dp),
-        colors = ButtonDefaults.buttonColors(containerColor = SecondPurple),
-        contentPadding = PaddingValues(start = 35.dp, end = 35.dp, top = 12.dp, bottom = 13.dp),
-        modifier = Modifier
-            .width(150.dp)
+//        shape = RoundedCornerShape(10.dp),
+//        colors = ButtonDefaults.buttonColors(containerColor = SecondPurple),
+//        contentPadding = PaddingValues(start = 35.dp, end = 35.dp, top = 12.dp, bottom = 13.dp),
+//        modifier = Modifier
+//            .width(150.dp)
     ) {
         Text(
             text = "Add New",
-            style = TextStyle(
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Medium
-            )
+//            style = TextStyle(
+//                fontSize = 18.sp,
+//                fontWeight = FontWeight.Medium
+//            )
         )
     }
 }
