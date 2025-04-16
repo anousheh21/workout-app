e27d5f6fb691edbd27fe1608bf088f2d28ff7c1d
anousheh21 <anousheh1@gmail.com>
2025-04-16 12:08:23 +0100

    Added instructions to WorkoutScreen, via No WorkoutScreen composable

--- app/src/main/java/com/example/workoutapp/ui/screens/WorkoutsScreen.kt
+++ app/src/main/java/com/example/workoutapp/ui/screens/WorkoutsScreen.kt
@@ -2,8 +2,13 @@ package com.example.workoutapp.ui.screens
 
 import android.util.Log
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -11,14 +16,19 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.workoutapp.data.Workout
 import com.example.workoutapp.ui.theme.SeparatorGrey
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -49,9 +59,51 @@ fun WorkoutsScreen(
         }
     }
 
-    WorkoutColumnList(workoutsArray, onClickWorkout)
+    if (workoutsArray.isEmpty()) {
+        NoWorkoutScreen()
+    } else {
+        WorkoutColumnList(workoutsArray, onClickWorkout)
+    }
+}
 
+@Composable
+fun NoWorkoutScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "How to Use This App",
+            style = TextStyle(
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold
+            )
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        InstructionText(text = "1. Navigate to the exercises tab, and add all exercises that you might carry out")
+        InstructionText(text = "2. Navigate to the schedule tab, and schedule in your workouts, adding the relevant exercises")
+        InstructionText(text = "3. Return to the workouts page, and click \"New Workout\" to start a new workout")
+    }
+}
 
+@Composable
+fun InstructionText(
+    text: String
+) {
+    Spacer(modifier = Modifier.height(15.dp))
+    Text(
+        text = text,
+        style = TextStyle(
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Normal,
+            lineHeight = 22.sp
+        ),
+        modifier = Modifier.fillMaxWidth(),
+        textAlign = TextAlign.Center
+    )
 
 }
 
