bcb132c529a35c98bc47b1cba51546b0cc2f92fa
anousheh21 <anousheh1@gmail.com>
2025-04-21 17:20:27 +0100

    Made detail screen scrollable

--- app/src/main/java/com/example/workoutapp/ui/screens/WorkoutDetailScreen.kt
+++ app/src/main/java/com/example/workoutapp/ui/screens/WorkoutDetailScreen.kt
@@ -1,12 +1,15 @@
 package com.example.workoutapp.ui.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -17,7 +20,13 @@ import com.example.workoutapp.ui.components.ExerciseLogSingle
 
 @Composable
 fun WorkoutDetailScreen(workoutId: Int) {
-    ExerciseLogSingle(workoutId)
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = Modifier.verticalScroll(scrollState)
+    ) {
+        ExerciseLogSingle(workoutId)
+    }
 }
 
 @Preview(showBackground = true)
